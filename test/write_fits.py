9fbc60ece943e921e15f476be6ec7ed3f9ada2bb
Graham Bell <g.bell@eaobservatory.org>
2018-11-21 14:07:35 -1000

    test: Update Astropy header update method usage

--- test/write_fits.py
+++ test/write_fits.py
@@ -46,76 +46,91 @@ def write_fits(filepath,
     # parse the filepath
     filebase = os.path.basename(filepath)
     file_id, ext = os.path.splitext(filebase)
-    hdu.header.update('FILE-ID', file_id)
-    hdu.header.update('COLLECT', 'TEST')
-    hdu.header.update('OBSID', obsid)
+    hdu.header.update({
+        'FILE-ID': file_id,
+        'COLLECT': 'TEST',
+        'OBSID': obsid,
 
-    # DPDATE will be different every time the program runs, so it should be
-    # possible to verify that the files have been updated in AD by checking
-    # this header.
-    hdu.header.update('DPDATE', datestring)
-    hdu.header.update('PRODUCT', product)
-    hdu.header.update('NUMEXTS', numexts)
-    hdu.header.update('FIELD1', 'F1%s' % (product))
-    hdu.header.update('FIELD2', 'F2%s' % (product))
+        # DPDATE will be different every time the program runs, so it should be
+        # possible to verify that the files have been updated in AD by checking
+        # this header.
+        'DPDATE': datestring,
+
+        'PRODUCT': product,
+        'NUMEXTS': numexts,
+        'FIELD1': 'F1%s' % (product),
+        'FIELD2': 'F2%s' % (product),
+    })
 
     if badheader:
-        hdu.header.update(badheader[0], badheader[1])
+        hdu.header.update({badheader[0]: badheader[1]})
 
     # Some product-dependent headers
     if product != 'A':
-        hdu.header.update('FIELD3', 'F3%s' % (product))
-        hdu.header.update('NOTA', True)
+        hdu.header.update({
+            'FIELD3': 'F3%s' % (product),
+            'NOTA': True,
+        })
     else:
-        hdu.header.update('NOTA', False)
+        hdu.header.update({
+            'NOTA': False,
+        })
 
     # Some extension-dependent headers
-    hdu.header.update('FIELD4', 'BAD')
-    hdu.header.update('FIELD5', 'GOOD')
+    hdu.header.update({
+        'FIELD4': 'BAD',
+        'FIELD5': 'GOOD',
+    })
 
     # Composite products have members identified by their file_id's
     if isinstance(member, list):
-        hdu.header.update('OBSCNT', len(member))
-        for i, name in enumerate(member):
-            hdu.header.update('OBS%d' % (i + 1), name)
+        hdu.header.update(OBSCNT=len(member))
+        hdu.header.update({'OBS%d' % (i + 1): name for i, name in enumerate(member)})
     elif isinstance(member, str):
-        hdu.header.update('OBSCNT', '1')
-        hdu.header.update('OBS1', member)
+        hdu.header.update(OBSCNT='1')
+        hdu.header.update(OBS1=member)
 
     # Derived products have inputs identified by their file_id's
     if isinstance(provenance, list):
-        hdu.header.update('PRVCNT', len(provenance))
-        for i, name in enumerate(provenance):
-            hdu.header.update('PRV%d' % (i + 1), name)
+        hdu.header.update(PRVCNT=len(provenance))
+        hdu.header.update({'PRV%d' % (i + 1): name for i, name in enumerate(provenance)})
     elif isinstance(provenance, str):
-        hdu.header.update('PRVCNT', '1')
-        hdu.header.update('PRV1', provenance)
+        hdu.header.update(PRVCNT='1')
+        hdu.header.update(PRV1=provenance)
 
     hdulist = fits.HDUList(hdu)
 
     # Optionally add extensions
     for extension in range(1, numexts + 1):
         hdu = fits.ImageHDU(data)
-        hdu.header.update('EXTNAME', 'EXTENSION%d' % (extension))
-        hdu.header.update('OBSID', obsid)
-        hdu.header.update('PRODUCT', '%s%d' % (product, extension))
-        hdu.header.update('DPDATE', datestring)
-        hdu.header.update('FIELD1', 'F1%s%d' % (product, extension))
-        hdu.header.update('FIELD2', 'F2%s%d' % (product, extension))
+        hdu.header.update({
+            'EXTNAME': 'EXTENSION%d' % (extension),
+            'OBSID': obsid,
+            'PRODUCT': '%s%d' % (product, extension),
+            'DPDATE': datestring,
+            'FIELD1': 'F1%s%d' % (product, extension),
+            'FIELD2': 'F2%s%d' % (product, extension),
+        })
 
         # Product dependent headers
         if product != 'A':
-            hdu.header.update('FIELD3', 'F3%s' % (product))
-            hdu.header.update('NOTA', True)
+            hdu.header.update({
+                'FIELD3': 'F3%s' % (product),
+                'NOTA': True,
+            })
         else:
-            hdu.header.update('NOTA', False)
+            hdu.header.update({
+                'NOTA': False,
+            })
 
         # Extension-dependent headers
-        hdu.header.update('FIELD4', 'GOOD')
-        hdu.header.update('FIELD5', 'BAD')
-        # an extension-specific header
-        hdu.header.update('HEADER%d' % (extension),
-                          'H%s%d' % (product, extension))
+        hdu.header.update({
+            'FIELD4': 'GOOD',
+            'FIELD5': 'BAD',
+
+            # an extension-specific header
+            'HEADER%d' % (extension): 'H%s%d' % (product, extension),
+        })
 
         hdulist.append(hdu)
 
